46515fca8285be61f06bd93539f54999dc871d7e
Lennon Correa <leennonc@gmail.com>
2022-05-31 21:46:47 -0300

    specifying types

--- components/more/more.tsx
+++ components/more/more.tsx
@@ -1,16 +1,17 @@
 import { motion, VariantLabels } from "framer-motion";
 import { Card, Col, Container, Image, ListGroup, ListGroupItem, Row } from "react-bootstrap";
 import styles from "./more.module.css"
-import { useState } from "react";
+import { FunctionComponent, useState } from "react";
 import { TwitchUserData } from "../../models/twitch";
 import { FaTwitch } from 'react-icons/fa';
+import { SteamUserData } from "../../models/steam";
 
-const More = ({ data }: { data: TwitchUserData }) => {
+const More = ({ twitchData, steamData }: { twitchData: TwitchUserData, steamData: SteamUserData }) => {
   const [twitchCardState, setTwitchCardState] = useState(false);
   const toggleTwichCard: any = () => {
     return twitchCardState ? setTwitchCardState(false) : setTwitchCardState(true);
   }
-  const createdAtFormated = new Date(data.created_at).formate();
+  const createdAtFormated = new Date(twitchData.created_at).formate();
   return (
     <>
       <Col className="centralizeY">
@@ -24,19 +25,19 @@ const More = ({ data }: { data: TwitchUserData }) => {
               <motion.div initial={{ opacity: 1 }} animate={twitchCardState ? { opacity: 1 } : { opacity: 1 }} className={styles.items}>
                 <ListGroup>
                   <motion.div initial={{ opacity: 0, display: 'none' }} animate={twitchCardState ? { opacity: 1, display: 'block' } : { opacity: 1 }} transition={{ delay: 0.15 }}>
-                    <ListGroup.Item className={styles.item}>{data.description.capitalize()}</ListGroup.Item>
+                    <ListGroup.Item className={styles.item}>{twitchData.description.capitalize()}</ListGroup.Item>
                   </motion.div>
                   <motion.div initial={{ opacity: 0, display: 'none' }} animate={twitchCardState ? { opacity: 1, display: 'block' } : { opacity: 1 }} transition={{ delay: 0.30 }}>
-                    <ListGroup.Item className={styles.item}>{"I'm a "}{data.broadcaster_type.capitalize()}</ListGroup.Item>
+                    <ListGroup.Item className={styles.item}>{"I'm a "}{twitchData.broadcaster_type.capitalize()}</ListGroup.Item>
                   </motion.div>
                   <motion.div initial={{ opacity: 0, display: 'none' }} animate={twitchCardState ? { opacity: 1, display: 'block' } : { opacity: 1 }} transition={{ delay: 0.45 }}>
                     <ListGroup.Item className={styles.item}>{"Account created at "}{createdAtFormated}</ListGroup.Item>
                   </motion.div>
                   <motion.div initial={{ opacity: 0, display: 'none' }} animate={twitchCardState ? { opacity: 1, display: 'block' } : { opacity: 1 }} transition={{ delay: 0.60 }}>
-                    <ListGroup.Item className={styles.item}>Currently at {data.view_count} views</ListGroup.Item>
+                    <ListGroup.Item className={styles.item}>Currently at {twitchData.view_count} views</ListGroup.Item>
                   </motion.div>
                   <motion.div initial={{ opacity: 0, display: 'none' }} animate={twitchCardState ? { opacity: 1, display: 'block' } : { opacity: 1 }} transition={{ delay: 0.60 }}>
-                    <ListGroup.Item className={styles.item}><Image src={data.profile_image_url} className={styles.profileImage} alt="twitch profile image" rounded/></ListGroup.Item>
+                    <ListGroup.Item className={styles.item}><Image src={twitchData.profile_image_url} className={styles.profileImage} alt="twitch profile image" rounded /></ListGroup.Item>
                   </motion.div>
                 </ListGroup>
               </motion.div>
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import React, { useState } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { GetStaticProps, NextPage } from 'next';
 
 import NavbarApp from '../components/navbar/navbar';
@@ -13,7 +13,8 @@ import { FaGit } from 'react-icons/fa';
 import { Container, Row, Col } from 'react-bootstrap';
 
 import styles from "../styles/portfolio.module.css"
-import { twitchUserData } from './api';
+import { steamUserData, twitchUserData } from './api';
+import { SteamUserData } from '../models/steam';
 
 const pageTitle: string = "Portfolio";
 const brand: object = <FaGit />;
@@ -25,69 +26,71 @@ const skills: object = {
   "React": "75"
 };
 
-const Portfolio: NextPage<TwitchUserData> = ({ user }: any) => {
-  const twitchUser: TwitchUserData = user;
-  const [homeState, setHomeState] = useState(true);
-  const [aboutState, setAboutState] = useState(false);
-  const [moreState, setMoreState] = useState(false);
-  const callback = (item: string) => {
-    if (item === "Home") {
-      setHomeState(true);
-      setAboutState(false);
-      setMoreState(false);
-      return;
+const Portfolio: ({ twitchData, steamData }: { twitchData: TwitchUserData; steamData: SteamUserData; }) => JSX.Element =
+  ({ twitchData, steamData }: { twitchData: TwitchUserData, steamData: SteamUserData }) => {
+    const [homeState, setHomeState] = useState(true);
+    const [aboutState, setAboutState] = useState(false);
+    const [moreState, setMoreState] = useState(false);
+    const callback = (item: string) => {
+      if (item === "Home") {
+        setHomeState(true);
+        setAboutState(false);
+        setMoreState(false);
+        return;
+      }
+      if (item === "About") {
+        setAboutState(true);
+        setMoreState(false);
+        setHomeState(false);
+        return;
+      }
+      if (item === "More") {
+        setMoreState(true);
+        setAboutState(false);
+        setHomeState(false);
+        return;
+      }
     }
-    if (item === "About") {
-      setAboutState(true);
-      setMoreState(false);
-      setHomeState(false);
-      return;
-    }
-    if (item === "More") {
-      setMoreState(true);
-      setAboutState(false);
-      setHomeState(false);
-      return;
-    }
-  }
-  return (
-    <>
-      <Head>
-        <title>{pageTitle}</title>
-        <meta name="description" content={"Lennon Correa portifolio in NextJs"} />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <Container>
-          <NavbarApp
-            brand={brand}
-            items={navItems}
-            callback={callback}
-          />
-          <Row className="block">
-            <Col className={homeState ? "centralizeY" : `${styles.responsiveHandle + " centralizeY"}`}>
-              <Introduce />
-            </Col>
-            {!homeState ?
+    return (
+      <>
+        <Head>
+          <title>{pageTitle}</title>
+          <meta name="description" content={"Lennon Correa portifolio in NextJs"} />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+          <Container>
+            <NavbarApp
+              brand={brand}
+              items={navItems}
+              callback={callback}
+            />
+            <Row className="block">
+              <Col className={homeState ? "centralizeY" : `${styles.responsiveHandle + " centralizeY"}`}>
+                <Introduce />
+              </Col>
+              {!homeState ?
                 <>
                   {aboutState ? <About skills={skills} /> : null}
-                  {moreState ? <More data={twitchUser} /> : null}
+                  {moreState ? <More twitchData={twitchData} steamData={steamData} /> : null}
                 </> :
-              null
-            }
-          </Row>
-          <Footer />
-        </Container>
-      </main>
-    </>
-  )
-};
+                null
+              }
+            </Row>
+            <Footer />
+          </Container>
+        </main>
+      </>
+    )
+  };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const data: TwitchUserData = await twitchUserData();
+  const twitchData: TwitchUserData = await twitchUserData();
+  const steamData: SteamUserData = await steamUserData();
   return {
     props: {
-      user: data
+      twitchData: twitchData,
+      steamData: steamData
     }
   }
 }

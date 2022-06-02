1704123c3d7e126ff6b8ca750b5128a011c2bd5f
Lennon Correa <leennonc@gmail.com>
2022-06-02 17:33:21 -0300

    animated list group type

--- components/animated-list-group/animatedListGroup.tsx
+++ components/animated-list-group/animatedListGroup.tsx
@@ -1,16 +1,18 @@
 import { motion } from "framer-motion";
 import { ListGroup } from "react-bootstrap";
 
-const AnimatedListGroup = ({ state, item, color, bgColor, delay }:
-  { state: boolean, item: any, color: string, bgColor: string, delay: number }) => {
-  const toggleOpacity = () => (
-    state ? { opacity: 1, display: 'block' } : { opacity: 0 }
-  );
-  return (
-    <motion.div initial={{ opacity: 0, display: 'none' }} animate={toggleOpacity()} transition={{ delay: delay }}>
-      <ListGroup.Item style={{ backgroundColor: bgColor, color: color }}>{item}</ListGroup.Item>
-    </motion.div>
-  )
-};
+const AnimatedListGroup: ({ state, item, color, bgColor, delay }:
+  { state: boolean; item: any; color: string; bgColor: string; delay: number; }) => JSX.Element =
+  ({ state, item, color, bgColor, delay }:
+    { state: boolean, item: any, color: string, bgColor: string, delay: number }) => {
+    const toggleOpacity = () => (
+      state ? { opacity: 1, display: 'block' } : { opacity: 0 }
+    );
+    return (
+      <motion.div initial={{ opacity: 0, display: 'none' }} animate={toggleOpacity()} transition={{ delay: delay }}>
+        <ListGroup.Item style={{ backgroundColor: bgColor, color: color }}>{item}</ListGroup.Item>
+      </motion.div>
+    )
+  };
 
 export default AnimatedListGroup;

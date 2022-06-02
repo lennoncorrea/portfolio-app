de39cafd8c3285edb3711d895b635c5eb1f15506
Lennon Correa <leennonc@gmail.com>
2022-06-02 17:30:36 -0300

    type animated card

--- components/animated-card/animatedCard.tsx
+++ components/animated-card/animatedCard.tsx
@@ -2,30 +2,32 @@ import { motion } from "framer-motion";
 import { Card, Container, ListGroup } from "react-bootstrap";
 import styles from "./AnimatedCard.module.css"
 
-const AnimatedCard = ({ listItems, header, state, color, bgColor, toggleState }:
-  { listItems: any, header: any, state: boolean, color: string, bgColor: string, toggleState: () => void }) => {
-  const stateOpacity = () => (
-    state ? { opacity: 1 } : { opacity: 0 }
-  );
-  return (
-    <>
-      <Container>
-        <Card className={styles.card}>
-          <Card.Header className={styles.header} onClick={() => toggleState()} style={{ backgroundColor: bgColor, color: color }} >
-            {header}
-          </Card.Header>
-          {state ?
-            <motion.div initial={{ opacity: 1 }} animate={stateOpacity()}>
-              <ListGroup>
-                {listItems}
-              </ListGroup>
-            </motion.div>
-            : null
-          }
-        </Card>
-      </Container>
-    </>
-  )
-};
+const AnimatedCard: ({ listItems, header, state, color, bgColor, toggleState }:
+  { listItems: any; header: any; state: boolean; color: string; bgColor: string; toggleState: () => void; }) => JSX.Element =
+  ({ listItems, header, state, color, bgColor, toggleState }:
+    { listItems: any, header: any, state: boolean, color: string, bgColor: string, toggleState: () => void }) => {
+    const stateOpacity = () => (
+      state ? { opacity: 1 } : { opacity: 0 }
+    );
+    return (
+      <>
+        <Container>
+          <Card className={styles.card}>
+            <Card.Header className={styles.header} onClick={() => toggleState()} style={{ backgroundColor: bgColor, color: color }} >
+              {header}
+            </Card.Header>
+            {state ?
+              <motion.div initial={{ opacity: 1 }} animate={stateOpacity()}>
+                <ListGroup>
+                  {listItems}
+                </ListGroup>
+              </motion.div>
+              : null
+            }
+          </Card>
+        </Container>
+      </>
+    )
+  };
 
 export default AnimatedCard;
--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -5,7 +5,7 @@ const Footer: () => JSX.Element = () => (
   <>
     <footer className={styles.footer}>
       <Link href="https://sonarcloud.io/summary/overall?id=lennoncorrea_portfolio-app">
-        <img src="https://sonarcloud.io/images/project_badges/sonarcloud-white.svg" className={styles.sonarCloud} alt="sonar image"></img>
+        <img src="https://sonarcloud.io/images/project_badges/sonarcloud-white.svg" className={styles.sonarCloud} alt="sonar image" />
       </Link>
     </footer>
   </>

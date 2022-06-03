effca940a2522e246401a84c1923c7c6cf47f7c0
Lennon Correa <leennonc@gmail.com>
2022-06-02 21:01:14 -0300

    changing img for Image

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
+import Image from 'next/image';
 import styles from "./footer.module.css"
 
 const Footer: () => JSX.Element = () => (
   <>
     <footer className={styles.footer}>
       <Link href="https://sonarcloud.io/summary/overall?id=lennoncorrea_portfolio-app">
-        <img src="https://sonarcloud.io/images/project_badges/sonarcloud-white.svg" className={styles.sonarCloud} alt="sonar image"></img>
+        <Image src="https://sonarcloud.io/images/project_badges/sonarcloud-white.svg" className={styles.sonarCloud} alt="sonar image"></Image>
       </Link>
     </footer>
   </>

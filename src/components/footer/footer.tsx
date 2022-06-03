8d41be74350d54f6322bff83b0bc91377ba512bf
Lennon Correa <leennonc@gmail.com>
2022-06-02 21:05:08 -0300

    Image for img

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
-import Image from 'next/image';
 import styles from "./footer.module.css"
 
 const Footer: () => JSX.Element = () => (
   <>
     <footer className={styles.footer}>
       <Link href="https://sonarcloud.io/summary/overall?id=lennoncorrea_portfolio-app">
-        <Image src="https://sonarcloud.io/images/project_badges/sonarcloud-white.svg" className={styles.sonarCloud} alt="sonar image"></Image>
+        <img src="https://sonarcloud.io/images/project_badges/sonarcloud-white.svg" className={styles.sonarCloud} alt="sonar image"></img>
       </Link>
     </footer>
   </>
